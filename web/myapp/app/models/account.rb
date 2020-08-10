fda29689938fe59e8d1e1e67e6006f2d429b15a6
TomoProg <helloworld0306.xxx@gmail.com>
2020-08-10 10:15:46 +0900

    ログイン機能を追加

---
+++ web/myapp/app/models/account.rb
@@ -0,0 +1,22 @@
+class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  attr_writer :login
+  def login
+    @login || self.email || self.username
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+end
