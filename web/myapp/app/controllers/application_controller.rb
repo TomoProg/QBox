395aad1883189de6fc7da9b2ec52d8ad19913ef0
TomoProg <helloworld0306.xxx@gmail.com>
2020-08-12 08:21:51 +0900

    質問の投稿機能を作成

--- web/myapp/app/controllers/application_controller.rb
+++ web/myapp/app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource) 
-    hello_path
+    questions_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
--- web/myapp/config/routes.rb
+++ web/myapp/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :questions
   root 'home#index'
 
   devise_for :accounts
