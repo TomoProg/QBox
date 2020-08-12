46895297f63ae6b06add562338b7f3cdb175e679
TomoProg <helloworld0306.xxx@gmail.com>
2020-08-12 11:57:23 +0900

    回答機能を作成

--- web/myapp/app/controllers/answers_controller.rb
+++ web/myapp/app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationController
+  def create
+    @answer = Answer.new(answer_params)
+    @question = Question.find(answer_params[:question_id])
+    if @answer.save
+      redirect_to @question, notice: 'Answer was successfully created.'
+    else
+      render 'questions/show'
+    end
+  end
+
+  private
+  # Only allow a list of trusted parameters through.
+  def answer_params
+    params.require(:answer).permit(:question_id, :contents).merge(account_id: current_account.id)
+  end
 end
