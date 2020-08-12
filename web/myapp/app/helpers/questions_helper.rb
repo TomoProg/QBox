8c20f2ad4d3bbbdeeedf2517969e4445d5e2ed8e
TomoProg <helloworld0306.xxx@gmail.com>
2020-08-12 15:58:44 +0900

    回答の削除は自分しかできないように変更

--- web/myapp/app/helpers/questions_helper.rb
+++ web/myapp/app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  # 指定されたレコードが自分の投稿かどうかを確認する
+  # @param model [Object] モデルクラス
+  # @param account_id [Integer] アカウントID
+  # @return [Boolean] 自分の投稿かどうか
+  def is_mine(model, account_id=current_account.id)
+    model.account_id == account_id
+  end
 end
